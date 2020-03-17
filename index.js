48caaabcc1a05f89ac11af0a89fb4d4f8671029e
Allen Ng <lukexng@gmail.com>
2020-03-17 23:29:00 +0800

    working version for android

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 import { NativeModules } from 'react-native'
 
-export default NativeModules.RNPytorch
+class RNPytorch {
+  async loadModel(modelPath, labelPath) {
+    await NativeModules.RNPytorch.loadModel(modelPath, labelPath)
+  }
+
+  async predict(imagePath) {
+    const result = await NativeModules.RNPytorch.predict(imagePath)
+    console.log(result)
+    const sum = result.reduce((acc, cur) => acc + Number(cur.confidence), 0)
+    const transformed = result.map((r, index) => {
+      return {
+        label: r.label,
+        confidence: Number(r.confidence) / sum,
+      }
+    })
+    return transformed
+  }
+}
+
+export default new RNPytorch()
